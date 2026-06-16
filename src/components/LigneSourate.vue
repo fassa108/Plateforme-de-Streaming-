fdb7d028364ae28cad5bf60ec9c114d62d15b979
bineta66 <binetabadadiane6@gmail.com>
2026-06-16 10:41:53 +0000

    Affichage des sourates album avec emit sur ligneSourate enfant de album

---
+++ src/components/LigneSourate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="ligne-sourate" @click="jouer">
+    <span class="numero">{{ piste.id }}</span>
+    <span class="titre">{{ piste.titre }}</span>
+    <span class="icone">▶</span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  piste: Object
+})
+
+const emit = defineEmits(['play'])
+
+function jouer() {
+  emit('play', props.piste)
+}
+</script>
--- src/pages/Album.vue
+++ src/pages/Album.vue
@@ -1,7 +1,52 @@
 <template>
+  <div class="album-view">
+    <router-link to="/">← Retour</router-link>
 
+    <div v-if="!album">
+      <p>Album introuvable</p>
+    </div>
+
+    <div v-else>
+      <div class="album-header">
+        <img v-if="album.cover" :src="album.cover" />
+        <div v-else class="cover-placeholder"></div>
+
+        <h1>{{ album.titre }}</h1>
+        <p>{{ album.recitateur }}</p>
+        <p>{{ album.genre }}</p>
+      </div>
+
+      <div class="liste-sourates">
+        <h2>Sourates</h2>
+
+        <LigneSourate
+          v-for="piste in album.pistes"
+          :key="piste.id"
+          :piste="piste"
+          @play="jouerPiste"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { albums } from '../data/albums.js'
+import LigneSourate from '../components/LigneSourate.vue'
 
+const route = useRoute()
+const album = ref(null)
+
+const emit = defineEmits(['play-track'])
+
+function jouerPiste(piste) {
+  emit('play-track', piste)
+}
+
+onMounted(() => {
+  const id = Number(route.params.id)
+  album.value = albums.find(a => a.id === id)
+})
 </script>
