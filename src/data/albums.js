81e832f85de4e87b5a01f420475bc1a2fa46015a
fassa108 <sambndeyefassa@gmail.com>
2026-06-17 10:52:49 +0000

    Ajout d'un nouvel album

--- src/data/albums.js
+++ src/data/albums.js
@@ -44,5 +44,30 @@ export const albums = [
         audio:"/audio/079.mp3"
       }
     ]
+  },
+  {
+    id:3,
+    titre:"Sourates Fatir (Le createur), Yaseen et Saffat (Les rangée)",
+    recitateur:"Sheikh Hussary",
+    cover:"/images/image3.png",
+    genre:"Récitation",
+
+    pistes:[
+      {
+        id:1,
+        titre:"AL Fatir",
+        audio:"/audio/035.mp3"
+      },
+      {
+        id:2,
+        titre:"Yaseen",
+        audio:"/audio/036.mp3"
+      },
+      {
+        id:3,
+        titre:"As Saafat",
+        audio:"/audio/037.mp3"
+      }
+    ]
   }
 ]
